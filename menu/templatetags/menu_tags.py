becad8b2a660444c3fee959b1c714bac22ea42e8
4uruH <97702288+4uruH@users.noreply.github.com>
2023-02-22 17:28:30 +0300

    thri

--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -7,6 +7,7 @@ register = template.Library()
 @register.inclusion_tag('menu/draw_menu.html')
 def draw_menu(filter=None):
     sub_menus = []
+    menu_title = ""
     data = SubMenu.objects.select_related('menu_title').filter(menu_title=filter)
     for el in data:
         sub_menus.append([el.title, el.url])
